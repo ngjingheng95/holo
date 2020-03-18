5a28d9bcf6a0d3d82f78615c16e3a995998fe6ac
ShiroTakeguchi <shirotimate@hotmail.com>
2020-03-18 17:48:32 +0800

    Navigation Bar removed

--- app/src/main/java/com/arholo/ar/sceneform/samples/chromakeyvideo/CreateArTutorial.java
+++ app/src/main/java/com/arholo/ar/sceneform/samples/chromakeyvideo/CreateArTutorial.java
@@ -1,10 +1,12 @@
 package com.arholo.ar.sceneform.samples.chromakeyvideo;
 
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -28,6 +30,19 @@ public class CreateArTutorial extends AppCompatActivity {
 
         viewFlipper.setFlipInterval(2000);
         viewFlipper.stopFlipping();
+
+        getWindow().getDecorView().getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                Rect rect = new Rect();
+                getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+
+
+                setToImmersiveMode();
+
+            }
+        });
     }
 
     public void onClickBackBtn(View view){
@@ -46,5 +61,16 @@ public class CreateArTutorial extends AppCompatActivity {
         viewFlipper.setInAnimation(this, R.anim.slide_in_right);
         viewFlipper.setOutAnimation(this, R.anim.slide_out_left);
         viewFlipper.showNext();
+    }
+
+    private void setToImmersiveMode() {
+        // set to immersive
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
--- app/src/main/java/com/arholo/ar/sceneform/samples/chromakeyvideo/camera/CameraActivity.java
+++ app/src/main/java/com/arholo/ar/sceneform/samples/chromakeyvideo/camera/CameraActivity.java
@@ -1,9 +1,11 @@
 package com.arholo.ar.sceneform.samples.chromakeyvideo.camera;
 
 import android.content.Intent;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ViewFlipper;
 
 
@@ -22,6 +24,19 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        getWindow().getDecorView().getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                Rect rect = new Rect();
+                getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+
+
+                setToImmersiveMode();
+
+            }
+        });
 
 
         // Initialise 'Help' Dialog
@@ -52,6 +67,17 @@ public class CameraActivity extends AppCompatActivity {
     public void onClickBackBtn(View view) {
         Intent intent = new Intent(CameraActivity.this, HoloHomepage.class);
         startActivity(intent);
+    }
+
+    private void setToImmersiveMode() {
+        // set to immersive
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     //public void onClickCameraHelpDialog(View view) {
